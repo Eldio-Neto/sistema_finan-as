6c1ad846204a8400fcb13192a569748b7ae8ee19
Eldio Neto <eldioneto2021@hmail.com>
2023-11-16 21:25:27 -0400

    projeto finalizado

---
+++ src/data/items.ts
@@ -0,0 +1,8 @@
+import { Item } from "../types/Item"
+
+export const items : Item[] = [
+    {date: new Date (2023, 10, 4), category: 'rent', title: 'Aluguel Apt', value: 32.12},
+    {date: new Date (2023, 10, 5), category: 'food', title: 'MacDonalds', value: 32.12},
+    {date: new Date (2023, 10, 5), category: 'food', title: 'Burger King', value: 32.12},
+    {date: new Date (2023, 10, 5), category: 'salary', title: 'Salario', value: 200}
+]
